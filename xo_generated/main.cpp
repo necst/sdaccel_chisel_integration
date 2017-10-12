2a6e0ce2bffb42cb22be3867f5dab41711326a8f
DavideConficconi <davideconficconi@gmail.com>
2017-10-12 15:30:53 +0200

    [Host] Host code fixed
    Signed-off-by: DavideConficconi <davideconficconi@gmail.com>

--- xo_generated/main.cpp
+++ xo_generated/main.cpp
@@ -54,92 +54,6 @@ ALL TIMES.
 #include <sys/stat.h>
 #include <CL/opencl.h>
 #include <CL/cl.h>
-//#include "support.h"
-////////////////////////////////////////////////////////////////////////////////
-
-// Use a static matrix for simplicity
-//
-#define MATRIX_RANK 16
-#define DATA_SIZE MATRIX_RANK*MATRIX_RANK
-
-#define N 256
-#define M 1048576
-
-const short GAP_i = -1;
-const short GAP_d = -1;
-const short MATCH = 2;
-const short MISS_MATCH = -1;
-const short CENTER = 0;
-const short NORTH = 1;
-const short NORTH_WEST = 2;
-const short WEST = 3;
-
-////////////////////////////////////////////////////////////////////////////////
-
-
-void set_char(unsigned int * array, int index, unsigned char val){
-				unsigned int temp = 0;
-				temp = val;
-
-				//databaseLocal[(N-1)/4] &= 4294967040;
-				//unsigned int mask = 16777215;
-				//mask = mask >> 8;
-				//array[index/4] = (array[index/4] & mask);
-				for(int i = (index % 4) * 8; i < ((index % 4) * 8) + 8; i++){
-					array[index/4] &= ~(1 << i);
-				}
-    			array[index / 4] |= (temp << ((index % 4) * 8));
-    			/*unsigned char val;
-    			unsigned int temp = array[index/32];
-    			val = (unsigned char) temp >> (index % 4) * 8;*/
-    		}
-
-
-void set_char_main(unsigned int  * array, int index, unsigned char val){
-				switch(val){
-					case 'A' : array[index / 16] |= (0 << ((index % 16) * 2));
-							//	array[index/16].range(((index % 16) * 2) + 1, ((index % 16) * 2)) = 0;
-							break;
-					case 'C' : array[index / 16] |= (1 << ((index % 16) * 2));
-						//	array[index/16].range(((index % 16) * 2) + 1, ((index % 16) * 2)) = 1;
-							break;
-					case 'G' : array[index / 16] |= (3 << ((index % 16) * 2));
-						//		array[index/16].range(((index % 16) * 2) + 1, ((index % 16) * 2)) = 3;
-							break;
-					case 'T' : array[index / 16] |= (2 << ((index % 16) * 2));
-						//		array[index/16].range(((index % 16) * 2) + 1, ((index % 16) * 2)) = 2;
-							break;
-				}
-    		}
-
-
-
-unsigned char get_char(unsigned int * array, int index){
-    	unsigned char val;
-    	unsigned int temp = array[index/4];
-    	val = (unsigned char) (temp >> ((index % 4) * 8));
-    	return val;
-    }
-
-
-short get(char data[], int key) {
-				const int position = (key % 4) * 2;
-				key /= 4;
-				char mask = 0;
-				mask &= 00000000;
-			
-				mask |= 3 << position;
-				
-				char fin_mask =0;
-				fin_mask&= 00000000;
-
-				fin_mask |= 3 << 0;
-				return (((data[key] & mask) >> ( position)) & fin_mask);
-				
-			}
-
-
-
 
 /*
 			Given an event, this function returns the kernel execution time in ms
@@ -162,127 +76,6 @@ short get(char data[], int key) {
 				total_time = time_end - time_start;
 				return total_time/ 1000000.0; // To convert nanoseconds to milliseconds
 			}
-
-unsigned short* order_matrix(unsigned short* in_matrix){
-	unsigned short * out_matrix = (unsigned short*)malloc(sizeof(unsigned short) * N * M);
-
-	int num_diag = 0;
-	int store_elem = 1;
-	int store_index;
-	int tmp_i = 0;
-	int tmp_j = 0;
-	int i;
-	int j;
-
-	for(i = 0; i< N * M;){
-		if(num_diag < M){
-			tmp_j = num_diag;
-			tmp_i = 0;
-		}else{
-			tmp_i = (num_diag + 1) - M;
-			tmp_j = M - 1;
-		}
-
-		for(j = 0; j < store_elem; j++){
-			store_index = tmp_j * N + tmp_i;
-			out_matrix[store_index] = in_matrix[i];
-			tmp_j--;
-			tmp_i++;
-			i++;
-		}
-		num_diag++;
-		if(num_diag >= M){
-			store_elem--;
-		}else{
-			if(store_elem != N) store_elem++;
-		}
-	}
-
-
-	return out_matrix;
-}
-
-
-unsigned short* order_matrix_blocks(unsigned short* in_matrix, unsigned short *out_matrix){
-  //unsigned short * out_matrix = (unsigned short*)malloc(sizeof(unsigned short) * N * M);
-
-  int num_diag = 0;
-  int store_elem = 1;
-  int store_index;
-  int tmp_i = 0;
-  int tmp_j = 0;
-  int i = 0;
-  int j;
-
-  for(i = 0; i<(M + N - 1) * N;){
-  //while(store_elem != 0){
-    if(num_diag < M){
-      tmp_j = num_diag;
-      tmp_i = 0;
-    }else{
-      tmp_i = (num_diag + 1) - M;
-      tmp_j = M - 1;
-    }
-
-    for(j = 0; j < store_elem; j++){
-      store_index = tmp_j * N + tmp_i;
-      out_matrix[store_index] = in_matrix[i];
-      //printf("stored %d in index %d \n", out_matrix[store_index], store_index);
-      tmp_j--;
-      tmp_i++;
-      i++;
-    }
-    num_diag++;
-    if(num_diag >= M){
-      store_elem--;
-      //i = num_diag * N + N - store_elem;
-      i = num_diag * N + N - store_elem;
-    }else{
-      if(store_elem != N){
-        store_elem++;
-        //i = num_diag * N;
-        i = num_diag * N;
-      }
-    }
-  }
-
-
-  return out_matrix;
-}
-
-    /* 
-    return a random number between 0 and limit inclusive.
-    */
-    int rand_lim(int limit) {
-      
-      int divisor = RAND_MAX/(limit+1);
-      int retval;
-
-      do { 
-        retval = rand() / divisor;
-      } while (retval > limit);
-
-      return retval;
-    }
-
-    /*
-    Fill the string with random values
-    */
-    void fillRandom(unsigned char* string, int dimension){
-        //fill the string with random letters.. 
-      static const char possibleLetters[] = "ATCG";
-
-      string[0] = '-';
-
-      int i;
-      for(i = 0; i<dimension; i++){
-        int randomNum = rand_lim(3);
-        string[i] = possibleLetters[randomNum];
-      }
-
-
-
-    }
 
 
 int
@@ -319,15 +112,9 @@ int main(int argc, char** argv)
 	unsigned int * b = (unsigned int *)malloc(sizeof(unsigned int) * 16);
 	unsigned int * out = (unsigned int *)malloc(sizeof(unsigned int) * 16);
 
-
-  unsigned int correct;               // number of correct results returned
-
 	printf("array defined! \n");
 	
 	fflush(stdout);
-
-  size_t global[2];                   // global domain size for our calculation
-  size_t local[2];                    // local domain size for our calculation
 
   cl_platform_id platform_id;         // platform id
   cl_device_id device_id;             // compute device id 
@@ -341,7 +128,6 @@ int main(int argc, char** argv)
    
   cl_mem input_a ;                     // device memory used for the input array
   cl_mem input_b ;                     // device memory used for the input array
-  cl_mem output ;                      // device memory used for the output array
   cl_mem out_buff;
    
   if (argc != 2){
@@ -482,18 +268,12 @@ int main(int argc, char** argv)
     return EXIT_FAILURE;
   }
 
-  // Create the input and output arrays in device memory for our calculation
-  //64*((N*(N+M-1)/256)+1)
-
-  //input_a = clCreateBuffer(context,  CL_MEM_READ_ONLY,  sizeof(unsigned char) * N, NULL, NULL);
 	printf("create buffer 0 \n");
   input_a = clCreateBuffer(context,  CL_MEM_READ_ONLY ,  sizeof(unsigned int) * 16, NULL, NULL);
-  //input_b = clCreateBuffer(context,  CL_MEM_READ_ONLY,  sizeof(unsigned char) * (M + 2*(N - 1)), NULL, NULL);
 	printf("create buffer 1 \n");
   input_b = clCreateBuffer(context,  CL_MEM_READ_ONLY ,  sizeof(unsigned int) * 16, NULL , NULL);
-  //output = clCreateBuffer(context, CL_MEM_WRITE_ONLY, sizeof(unsigned short) * N*(N+M-1), NULL, NULL);
 	printf("create buffer 2 \n");
-  out_buff = clCreateBuffer(context, CL_MEM_READ_WRITE, sizeof(unsigned int)* N, NULL, NULL);
+  out_buff = clCreateBuffer(context, CL_MEM_READ_WRITE, sizeof(unsigned int)* 16, NULL, NULL);
   
 if (!input_a || !input_b || !out_buff) 
   {
@@ -530,21 +310,9 @@ if (!input_a || !input_b || !out_buff)
     return EXIT_FAILURE;
   }
 
-  // Execute the kernel over the entire range of our 1d input data set
-  // using the maximum number of work group items for this device
-  //
 cl_event enqueue_kernel;
-#ifdef C_KERNEL
 	printf("LAUNCH task \n");
   err = clEnqueueTask(commands, kernel, 0, NULL, &enqueue_kernel);
-#else
-  global[0] = MATRIX_RANK;
-  global[1] = MATRIX_RANK;
-  local[0] = MATRIX_RANK;
-  local[1] = MATRIX_RANK;
-  err = clEnqueueNDRangeKernel(commands, kernel, 2, NULL, 
-                               (size_t*)&global, (size_t*)&local, 0, NULL, NULL);
-#endif
   if (err)
   {
     printf("Error: Failed to execute kernel! %d\n", err);
@@ -572,19 +340,9 @@ cl_event enqueue_kernel;
   clWaitForEvents(1, &readevent);
 
 	float executionTime = getTimeDifference(enqueue_kernel); 
-    	/*
-	unsigned short tempMatrix[256*(N+M-1)]; //do not think the *4 is needed
-	for(int i = 0; i<256*(N+M-1); i++){
-		tempMatrix[i] = get(results2,i);
-		//printf("read %d at index %d \n", tempMatrix[i], i);
-	}
-	*/
 	
 	    
 	printf(" execution time is %f ms\n", executionTime);
-  for(int i = 0; i < 16; i++){
-      printf("READ %d \n", out[i]);
-    }
     
   // Print a brief summary detailing the results
   //
@@ -594,7 +352,7 @@ cl_event enqueue_kernel;
   //
   clReleaseMemObject(input_a);
   clReleaseMemObject(input_b);
-  clReleaseMemObject(output);
+  clReleaseMemObject(out_buff);
   clReleaseProgram(program);
   clReleaseKernel(kernel);
   clReleaseCommandQueue(commands);
