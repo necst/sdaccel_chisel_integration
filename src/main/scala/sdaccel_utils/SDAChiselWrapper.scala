388f3080a3f352ad03cd7bdd9206cd6f8536a383
Lorenzo Di Tucci <lorenzo.ditucci@mail.polimi.it>
2017-10-19 18:45:11 +0200

    this way is more correct, as suggest in the issue

--- src/main/scala/sdaccel_utils/SDAChiselWrapper.scala
+++ src/main/scala/sdaccel_utils/SDAChiselWrapper.scala
@@ -36,8 +36,8 @@ class SDAChiselWrapper(addrWidth : Int, dataWidth : Int, idBits : Int, dataWidth
   //
 
 
-  slave_fsm.reset := !(reset.toBool())
-  RTLKernel.reset := !(reset.toBool())
+  slave_fsm.reset := (!reset.toBool()).asTypeOf(reset)
+  RTLKernel.reset := (!reset.toBool()).asTypeOf(reset)
 
   slave_fsm.io.slave.writeAddr.bits.prot := io.s0.writeAddr.bits.prot
   slave_fsm.io.slave.writeAddr.bits.addr := io.s0.writeAddr.bits.addr
